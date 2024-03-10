ffc0d930c197837a2db67720029dc14c1729df65
Khamidullin Rustam <rstm.khamidullin@gmail.com>
2024-03-10 18:20:59 +0700

    ready for PR

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,17 +1,29 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import lombok.SneakyThrows;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Testcontainers
 public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
     static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:15")
+        POSTGRES = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("scrapper")
             .withUsername("postgres")
             .withPassword("postgres");
@@ -20,8 +32,31 @@ public abstract class IntegrationTest {
         runMigrations(POSTGRES);
     }
 
+    @SneakyThrows
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        try (Connection connection = DriverManager.getConnection(
+            c.getJdbcUrl(),
+            c.getUsername(),
+            c.getPassword()
+        )
+        ) {
+            Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Path changelogPath = new File("").toPath()
+                .toAbsolutePath()
+                .getParent()
+                .resolve("migrations");
+
+            Liquibase liquibase =
+                new Liquibase(
+                    "master.xml",
+                    new DirectoryResourceAccessor(changelogPath),
+                    database
+                );
+
+            liquibase.update(new Contexts(), new LabelExpression());
+        }
     }
 
     @DynamicPropertySource
